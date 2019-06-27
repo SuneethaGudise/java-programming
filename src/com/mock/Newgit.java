8424d031069d01b34e3fc6712bf60f921220086f
suneethagudise <suneetha.gudise@gmail.com>
2019-06-27 16:41:07 -0700

    added newgit

---
+++ src/com/mock/Newgit.java
@@ -0,0 +1,35 @@
+package com.mock;
+
+public class Newgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//RectangleArea ra=new RectangleArea(0, 0);
+		
+	//	RectangleArea ar1= new RectangleArea();
+		RectangleArea ar = new RectangleArea(40,5);
+		
+		int result = ar.Area();
+		 Area1.displayRec1(ar);
+		
+	
+		System.out.println(result);
+				
+	}
+	
+	// display only when len > 50
+	public static void displayRec1(RectangleArea obj ) {
+		RectangleArea temp=obj;
+		
+		int len = obj.getLength();
+		if (len > 50)
+			System.out.println("displaying rect");
+		//return temp;
+	 }
+	
+	 public int displayRec(int len,int wid ) {
+		 int a=len*wid;
+		 return a;
+	 }
+	
+}
